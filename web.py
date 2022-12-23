ef78ff69b187de1e27e482473884379d77a1bf5c
levshadrin <leo.shadrin@gmail.com>
2022-12-23 12:11:48 +0100

    Implemented DB, REST, and search

--- web.py
+++ web.py
@@ -1,19 +1,39 @@
+import os
+import requests
+
 from flask import Flask, render_template, request
-# import requests
+
+REST_URL = os.environ.get ("REST_URL", "http://localhost/")
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return 'Hello, World!'
+def gspc_home():
+    return render_template('index.html.j2')
 
-# @app.route("/", methods=['GET', 'POST'])
-# def hello_world():
-#     if request.method == 'POST':
-#         firstname = request.values.get('firstname')
-#         lastname = request.values.get('lastname')
-#         requests.post(REST_URL, json={'firstname':firstname, 'lastname':lastname})
-#     result = requests.get(REST_URL).json()
-    # return render_template("index.html.j2", result=result)
+def render_search_table(q=None):
+    people_items = requests.get(REST_URL).json().get("_items")
+    if q:
+        people_items = [i for i in people_items \
+            if q.lower() in i.get("firstname").lower() \
+            or q.lower() in i.get("lastname").lower()]
+    return render_template("search_table.html.j2", people_items=people_items)
+
+@app.route("/people", methods=["GET", "POST"])
+def gspc_people():
+    if request.method == "POST":
+        firstname = request.values.get("firstname")
+        lastname = request.values.get("lastname")
+        if firstname and lastname:
+            requests.post(REST_URL, json={"firstname": firstname, "lastname": lastname}) 
+    search_table = render_search_table()
+    return render_template("people.html.j2", search_table=search_table)
+
+@app.route("/search", methods=["GET"]) 
+def startup_search():
+    q = request.values.get("q")
+    search_table = render_search_table(q=q)
+    return render_template("search.html.j2", search_table=search_table, q=q)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
